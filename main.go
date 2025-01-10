7630ac5d4fdf24bd8bb4327c4c04310e9720dd1f
ModnyDed <workstudy135@gmail.com>
2025-01-10 10:55:15 +0300

    реализация работы клиента

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+func main() {
+	tgClient = telegram.New(tgBotHost, mustToken())
+
+	// fethcer = fetcher.New(tgClient)
+
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetcher, processor)
+
+}
+
+func mustToken() (string, error) {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+
+}
